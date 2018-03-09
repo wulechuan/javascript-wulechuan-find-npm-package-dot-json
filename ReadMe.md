435ac6e07914a7baa1f3a7150139e459fad5aaf6
wulechuan <wulechuan@live.com>
2018-03-09 17:52:45 +0800

    First commit

---
+++ ReadMe.md
@@ -0,0 +1,94 @@
+# NPM Page
+
+[@wulechuan/find-package-dot-json](https://www.npmjs.com/package/@wulechuan/find-package-dot-json)
+
+<br/>
+<br/>
+<br/>
+<br/>
+
+# Introduction
+
+Search folder tree **upwards** and locate the nearest npm project **root** folder, returning the parsed `package.json` as an object.
+
+<br/>
+<br/>
+
+# Usage
+
+```javascript
+function ensureCWDToBeNPMProjectRootAndReturnPackageJSON(options) {
+
+
+
+	/* ********************************************* */
+
+	const result = require('@wulechuan/find-package-dot-json')({
+		checkingStartPoint:    '<a path to start with>',             // optional
+		desiredNPMProjectName: '<your fancy npm project name here>', // optional
+    });
+
+	/* ********************************************* */
+
+
+
+    // If fail to find one, throw an error
+	if (! result) {
+		throw ReferenceError('Fail to locate npm project root.');
+	}
+
+
+    // If not thrown, now we are safe to move on.
+	const {
+		npmProjectRootPath,
+	} = result;
+
+
+	process.chdir(npmProjectRootPath);
+
+
+    // By the way, the line below simulates a gulp log.
+	console.log(`[${
+		chalk.gray(moment().format('HH:mm:ss'))
+	}] Working directory changed to\n${' '.repeat('[HH:mm:ss] '.length)}${
+		chalk.green(process.cwd())
+	}\n\n\n`);
+
+
+	return result;
+}
+```
+
+
+<br/>
+<br/>
+<br/>
+<br/>
+
+# API
+
+## arguments
+
+```javascript
+{
+	checkingStartPoint:    '<a path to start with>',             // optional
+	desiredNPMProjectName: '<your fancy npm project name here>', // optional
+}
+```
+
+<br/>
+<br/>
+
+## return value
+
+```javascript
+{
+    npmProjectRootPath: 'a string',
+    packageJSON: 'an object',
+}
+```
+
+<br/>
+<br/>
+<br/>
+<br/>
---
+++ source/index.js
@@ -0,0 +1,79 @@
+const fileSytem = require('fs');
+const pathTool = require('path');
+
+const { join: joinPath } = pathTool;
+
+module.exports = function findNPMProjectRootFolderAndPackageJSON(options = {}) {
+	const {
+		checkingStartPoint = process.cwd(),
+		desiredNPMProjectName = '',
+	} = options;
+
+
+	if (desiredNPMProjectName) {
+		throwIfNPMProjectNameSeemsInvalid(desiredNPMProjectName);
+	}
+
+
+	let currentCheckingPath = checkingStartPoint;
+	let packageJSON;
+
+	while (
+		! (packageJSON = foundValidPackageJSON(currentCheckingPath, desiredNPMProjectName)) &&
+		! folderIsTopMostOne(currentCheckingPath)
+	) {
+		currentCheckingPath = pathTool.resolve(currentCheckingPath, '..');
+	}
+
+	if (! packageJSON) {
+		return null;
+	}
+
+	return {
+		npmProjectRootPath: currentCheckingPath,
+		packageJSON,
+	};
+};
+
+
+function throwIfNPMProjectNameSeemsInvalid(projectName) { // https://docs.npmjs.com/files/package.json#name
+	if (
+		! (
+			(
+				projectName.match(/@[a-z]+\/[a-z_-]+/) ||
+				projectName.match(/[a-z]+[a-z_-]+/)
+			)
+
+			&&
+
+			projectName.length < 215
+		)
+	) {
+		throw RangeError('NPM project name should only contains lowercase letters, "@", "-", "_", or "/".');
+	}
+}
+
+function foundValidPackageJSON(folder, desiredNPMProjectName) {
+	const foundPackageJsonFullPath = joinPath(folder, 'package.json');
+
+	if (fileSytem.existsSync(foundPackageJsonFullPath)) {
+		const packageJSON = require(foundPackageJsonFullPath); // eslint-disable-line import/no-dynamic-require
+
+		if (packageJSON && typeof packageJSON === 'object') {
+			if (! desiredNPMProjectName) { // If it's not specified, that means any project name counts.
+				return packageJSON;
+			}
+
+			if (packageJSON.name === desiredNPMProjectName) { // We've found the project!
+				return packageJSON;
+			}
+		}
+	}
+
+	return null; // Sorry! Not this one.
+}
+
+function folderIsTopMostOne(folder) {
+	const segments = folder.split(pathTool.sep);
+	return segments.length === 2 && segments[1].length === 0;
+}
